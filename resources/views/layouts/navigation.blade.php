65b9439e3fc2a81c8348d3313f44660c41105df2
chrmn6 <charmainerubiato2017@gmail.com>
2025-09-22 14:44:40 +0800

    changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,38 +1,38 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ url('/dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto text-gray-800" />
+                    <a href="{{ route('dashboard') }}">
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="url('/dashboard')" :active="request()->is('dashboard')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="url('/activities')" :active="request()->is('activities')">
+                    <x-nav-link :href="route('activities')" :active="request()->routeIs('activities')">
                         {{ __('Activities') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="url('/announcement')" :active="request()->is('announcement')">
+                    <x-nav-link :href="route('announcement')" :active="request()->routeIs('announcement')">
                         {{ __('Announcement') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="url('/services')" :active="request()->is('services')">
+                    <x-nav-link :href="route('services')" :active="request()->routeIs('services')">
                         {{ __('Services') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="url('/service-request')" :active="request()->is('service-request')">
+                    <x-nav-link :href="route('service-request')" :active="request()->routeIs('service-request')">
                         {{ __('Service Request') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="url('/emergency')" :active="request()->is('emergency')">
+                    <x-nav-link :href="route('emergency')" :active="request()->routeIs('emergency')">
                         {{ __('Contact Info') }}
                     </x-nav-link>
                 </div>
@@ -67,7 +67,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                            this.closest('form').submit();">
+                                                            throuteIs.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
@@ -94,7 +94,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routerouteIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
@@ -116,7 +116,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                            this.closest('form').submit();">
+                                            throuteIs.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
