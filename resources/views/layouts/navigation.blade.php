0b1ba24aa7d51e5f8e8897a57214c4bd1d1f7359
chrmn6 <charmainerubiato2017@gmail.com>
2025-09-22 20:31:18 +0800

    changes in logout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -67,7 +67,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
-                                                            throuteIs.closest('form').submit();">
+                                                            this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
@@ -116,7 +116,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                            throuteIs.closest('form').submit();">
+                                            this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
