bd4bb193d84e6e940163bfdbbc3ef560e26218e1
chrmn6 <charmainerubiato2017@gmail.com>
2025-09-25 20:38:26 +0800

    navbar to sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,23 +17,19 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        @include('layouts.navigation')
+<body class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen flex">
+        @include('layouts.navigation') <!-- Sidebar -->
 
-        <!-- Page Heading -->
-        @isset($header)
-            <header class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endisset
+        <div class="flex-1 ml-64">
+            @isset($header)
+                <header class="bg-white dark:bg-gray-800 shadow mb-6">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{{ $header }}</div>
+                </header>
+            @endisset
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+            <main class="p-6">{{ $slot }}</main>
+        </div>
     </div>
 </body>
 
